8f5877ef0f255327eae5ee4a5cf2f5e49fc108be
Jay Dave <jdave@claritycon.com>
2016-02-22 12:18:03 -0600

    adding exisiting code stucture - finished routes for admin and index using namespace ADMIN

---
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SimpleBlog.Controllers;
+
+namespace SimpleBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var namespaces = new[] {typeof (PostsController).Namespace}; // creating namespace to allow for multiple controllers with the same name (ie index PostsController and namespace Admin, PostsController
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces); // Login Route
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces); // Index Route
+        }
+    }
+}
